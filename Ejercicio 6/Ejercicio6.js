bdc0ae375753692e70d18f207aed6841a64d1ad9
Ismael Malanga <genaromalanga@gmail.com>
2025-05-23 11:50:33 -0300

    Ejercicio 6 listo y agregado

---
+++ Ejercicio 6/Ejercicio6.js
@@ -0,0 +1,35 @@
+const nombre = document.getElementById("nombre")
+const apellido = document.getElementById("apellido")
+const edad = document.getElementById("edad")
+const altura = document.getElementById("altura")
+const correo = document.getElementById("correo")
+const mensaje = document.getElementById("mensaje")
+
+
+function validar (){
+
+    let errores = "";
+
+    if (nombre.value === "" || nombre.value.length > 50) {
+        errores = "nombre invalido.";
+    } else if (apellido.value === "" || apellido.value.length > 50) {
+        errores = "apellido invalido.";
+    } else if (edad.value < 0) {
+        errores = "la edad no tiene que ser negativa.";
+    } else if (edad.value < 18) {
+        errores = "debe ser mayor de edad.";
+    } else if (altura.value < 0 || altura.value > 230) {
+        errores = "altura invalida.";
+    } else if (correo.value === "" || !correo.value.includes("@")) {
+        errores = "correo invalido.";
+    }
+
+    if (errores === "") {
+        mensaje.innerHTML = "los datos son validos, formulario enviado";
+        mensaje.style.color = "green";
+    } else {
+        mensaje.innerHTML = errores;
+        mensaje.style.color = "red";
+    }
+
+}
